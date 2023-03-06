216caf28dacc330c9018ec324ea3d333be08f326
Kirill <dorc99@mail.ru>
2023-03-06 23:35:16 +0700

    добавил анимацию открытия попапа

--- scripts/index.js
+++ scripts/index.js
@@ -6,6 +6,7 @@ const inputSubtitleElement = popupElement.querySelector(".form__item_el_subheadi
 const profileFullNameElement = document.querySelector(".profile__full-name");
 const profileProfessionElement = document.querySelector(".profile__profession");
 const formElement = popupElement.querySelector(".form");
+const profileAddBtnElement = document.querySelector(".profile__add-button");
 
 // Реализация функционала ПР4
 
@@ -34,29 +35,4 @@ popupCloseBtnElement.addEventListener("click", closePopupProfile);
 
 //Реализация функционала ПР5
 
-const initialCards = [ // Набор карточек
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-]; 
+
