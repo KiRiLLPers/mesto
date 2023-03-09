8849850694427ae72fe5e2e5383c425b74d2e552
Kirill <dorc99@mail.ru>
2023-03-09 23:58:17 +0700

    добавлен рендеринг карточек при загрузке страницы

--- scripts/index.js
+++ scripts/index.js
@@ -21,8 +21,35 @@ const formCardsElement = document.querySelector(".popup-form-cards");
 const inputCardsTtileElement = formCardsElement.querySelector(".form__item_el_place-name");
 const inputCardsSubTtileElement = formCardsElement.querySelector(".form__item_el_url");
 
+const divPhotosElement = document.querySelector(".photos__wrap")
 
-const cardLikeElements = document.querySelectorAll(".card__like-image");
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
 
 const openPopup = (popup) => { // функция открытия любого попапа
   popup.classList.add("popup_opened");
@@ -61,6 +88,41 @@ const openPopupCards = () => {
 }
 
 
+const renderCards = (arrCards) => {
+
+  arrCards.forEach((item) => {
+    const cardTemplate = document.querySelector("#card-template").content;
+    const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+
+    cardElement.querySelector(".card__image").src = item.link;
+    cardElement.querySelector(".card__image").alt = item.name;
+    cardElement.querySelector(".card__title").textContent = item.name;
+
+    divPhotosElement.append(cardElement);
+
+    cardElement.querySelector(".card__like-image").addEventListener("click", (e) => {
+      e.target.classList.toggle("card__like-image_active");
+    })
+
+  })
+}
+
+const handleFormSubmitCards = (e) => { // обработчик сохранения новых карточек
+  e.preventDefault();
+
+  const newCard = {};
+
+  newCard.name = inputCardsTtileElement.value;
+  newCard.link = inputCardsSubTtileElement.value;
+
+  closePopup(popupCardsElement);
+}
+
+
+renderCards(initialCards);
+
+
+// слушатели
 popupElement.forEach((item) => {
   item.addEventListener("click", closePopupClickOnOverlay);
 });
@@ -68,6 +130,7 @@ popupElement.forEach((item) => {
 profileBtnElement.addEventListener("click", openPopupProfile); // слушатель для открытия редактирования профиля
 formProfileElement.addEventListener("submit", handleFormSubmitProfile); // слушатель для обработки введенных пользователей данных и клика на "сохранить"
 popupCardsOpenBtnElement.addEventListener("click", openPopupCards) // слушатель для открытия попапа добавления новых карточек
+formCardsElement.addEventListener("submit", handleFormSubmitCards);
 
 
 popupCloseBtnElement.forEach((item) => { // закрываем любой попап нажатием на любой крестик закрытия попапа)
