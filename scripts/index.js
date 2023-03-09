5d7680f48f52ff690ceb797c9041a852be4de1a0
Kirill <dorc99@mail.ru>
2023-03-09 22:50:06 +0700

    Реализовать открытие и закрытие попапов

--- scripts/index.js
+++ scripts/index.js
@@ -24,40 +24,40 @@ const inputCardsSubTtileElement = formCardsElement.querySelector(".form__item_el
 
 const cardLikeElements = document.querySelectorAll(".card__like-image");
 
+const openPopup = (popup) => { // функция открытия любого попапа
+  popup.classList.add("popup_opened");
+}
 
+const closePopup = (popup) => { // функция закрытия любого попапа
+  popup.classList.remove("popup_opened");
+}
 
-const openPopupProfile = (e) => {
-  popupProfileElement.classList.add("popup_opened");
+const closePopupClickOnOverlay = (e) => { // функция закрытия любого попапа кликом на оверлей
+  if(e.target === e.currentTarget) {
+    popupElement.forEach((item) => {
+      closePopup(item);
+    })
+  }
+}
+
+const openPopupProfile = () => { // функция открытия попапа с редактированием профиля пользователя
+  openPopup(popupProfileElement);
 
   inputProfileTtileElement.value = profileFullNameElement.textContent;
   inputProfileSubtitleElement.value = profileProfessionElement.textContent;
 }
 
-const openPopupCards = (e) => {
-  popupCardsElement.classList.add("popup_opened");
-}
-
-
-const closePopup = () => { 
-  popupElement.forEach((item) => {
-    item.classList.remove("popup_opened");
-  });
-}
-
-const closePopupClickOnOverlay = (e) => {
-  if(e.target === e.currentTarget) {
-    closePopup();
-  }
-
-}
-
-const handleFormSubmitProfile = (e) => {
+const handleFormSubmitProfile = (e) => { // обработчик введенных пользователей данных для редактирования профиля
   e.preventDefault();
 
   profileFullNameElement.textContent = inputProfileTtileElement.value;
   profileProfessionElement.textContent = inputProfileSubtitleElement.value;
 
-  closePopup();
+  closePopup(popupProfileElement);
+}
+
+const openPopupCards = () => {
+  openPopup(popupCardsElement);
 }
 
 
@@ -65,13 +65,18 @@ popupElement.forEach((item) => {
   item.addEventListener("click", closePopupClickOnOverlay);
 });
 
-profileBtnElement.addEventListener("click", openPopupProfile);
-formProfileElement.addEventListener("submit", handleFormSubmitProfile);
+profileBtnElement.addEventListener("click", openPopupProfile); // слушатель для открытия редактирования профиля
+formProfileElement.addEventListener("submit", handleFormSubmitProfile); // слушатель для обработки введенных пользователей данных и клика на "сохранить"
+popupCardsOpenBtnElement.addEventListener("click", openPopupCards) // слушатель для открытия попапа добавления новых карточек
 
-popupCloseBtnElement.forEach((item) => {
-  item.addEventListener("click", closePopup);
+
+popupCloseBtnElement.forEach((item) => { // закрываем любой попап нажатием на любой крестик закрытия попапа)
+  item.addEventListener("click", () => {
+    popupElement.forEach((popupEl) => {
+      closePopup(popupEl);
+    })
+  })
 })
-popupCardsOpenBtnElement.addEventListener("click", openPopupCards);
 
 
 
